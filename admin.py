5b6db9a6dd2f2ff5c96b52e6ec99914295257e12
Abd <abdullahghzaill4@gmail.com>
2021-06-03 09:54:55 -0700

    Add project

--- admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Resturant
-admin.site.register(Resturant)
-# Register your models here.
---
+++ resturants/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Resturant
+
+admin.site.register(Resturant)
+# Register your models here.
